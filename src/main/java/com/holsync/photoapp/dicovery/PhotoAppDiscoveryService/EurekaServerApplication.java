ed2ccbc63410e9addcea85f7cebec0211d21c223
sachinhol <sachinhol91@gmail.com>
2025-03-22 22:13:03 +0530

    Eureka Web Service Integrated

--- src/main/java/com/holsync/photoapp/dicovery/PhotoAppDiscoveryService/EurekaServerApplication.java
+++ src/main/java/com/holsync/photoapp/dicovery/PhotoAppDiscoveryService/EurekaServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class PhotoAppDiscoveryServiceApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PhotoAppDiscoveryServiceApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
